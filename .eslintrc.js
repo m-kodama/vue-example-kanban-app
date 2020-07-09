711223c7d1e3a07889e3f8ace4f0113ad7f212e0
Masaaki Kodama <tc003ssr@gmail.com>
2020-07-09 22:20:37 +0900

    feat: vue-cli-plugin-express追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["vue"],
   rules: {
@@ -14,17 +14,17 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "import/no-webpack-loader-syntax":
-      process.env.NODE_ENV === "production" ? "error" : "off",
+      process.env.NODE_ENV === "production" ? "error" : "off"
   },
   overrides: [
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
-        jest: true,
-      },
-    },
-  ],
+        jest: true
+      }
+    }
+  ]
 };
