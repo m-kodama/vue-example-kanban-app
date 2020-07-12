a2b7a1d35e33f5b5481c7a003113b196596627b3
Masaaki Kodama <tc003ssr@gmail.com>
2020-07-13 01:25:42 +0900

    feat: vuex関連ファイルを追加

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import actions from "./actions";
+import getters from "./getters";
+import mutations from "./mutations";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  getters,
+  actions,
+  mutations,
+  strict: process.env.NODE_ENV !== "production",
 });
